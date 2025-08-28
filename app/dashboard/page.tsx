9d8240d232b108265f7db30bb3500d525836815d
Chaitanya Nevse <chaitanyanevse3012@gmail.com>
2025-08-28 19:30:11 +0530

    created  data-page component.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,6 +6,8 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { ButtonIcon } from "@/components/ui/iconButton";
+import DataPage from "./data-page";
+
 export default function Page() {
   return (
     <SidebarProvider>
@@ -21,13 +23,9 @@ export default function Page() {
           </div>
           <ButtonIcon className="mr-4" />
         </header>
+        {/*Main Content*/}
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          <DataPage />
         </div>
       </SidebarInset>
     </SidebarProvider>
